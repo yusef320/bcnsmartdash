8e3c03d3c02621466eee3cb27885c51a90367db6
yusef320 <82632877+yusef320@users.noreply.github.com>
2023-06-20 20:36:05 +0200

    Added routing

--- web.py
+++ web.py
@@ -1,12 +1,16 @@
 import requests
 import folium
-from streamlit_folium import folium_static
+import streamlit_folium as stf
 import streamlit as st
 
 
+
+
 def get_marker_color(disp):
-    if disp > 0:
+    if disp > 1:
         return 'green'
+    elif disp ==1:
+        return 'orange'
     else:
         return 'red'
 
@@ -19,14 +23,42 @@ def get_marker_popup(station):
     horario = station['horario']
     acceso = station['acceso']
 
-    popup = f"<b>Propietario:</b> {lab}<br>"
-    popup += f"<b>Dirección:</b> {dir}<br>"
-    popup += f"<b>Cargadores disponibles:</b> {num}<br>"
-    popup += f"<b>Apertura: </b> {dias}<br>"
-    popup += f"<b>Horario: </b> {horario}<br>"
-    popup += f"<b>Tipo de propiedad: </b> {acceso}<br>"
+    popup = f"<b>Owner:</b> {lab}<br>"
+    popup += f"<b>Address:</b> {dir}<br>"
+    popup += f"<b>Available chargers:</b> {num}<br>"
+    popup += f"<b>Opening days: </b> {dias}<br>"
+    popup += f"<b>Hours : </b> {horario}<br>"
+    popup += f"<b>Type of access: </b> {acceso}<br>"
 
     return popup
+
+
+
+st.set_page_config(
+    page_title="eBCN",
+    layout="wide",
+)
+
+
+
+st.markdown("# eBCN chargers", unsafe_allow_html=True)
+
+
+
+
+col1, col2 = st.columns([3,1])
+with col1:
+    dire = st.text_input('Introduce an origin to get the route to the closet available charger', 'Example: Av. Diagonal, 467')
+with col2:
+    type_charger = st.selectbox(
+        ' ',
+        ('Atleast one available connector', 'More than one available connector'))
+
+
+if type_charger == "Atleast one available connector":
+    n_charg = 1
+else:
+    n_charg = 2
 
 
 # Load the JSON data from the provided URL
@@ -35,8 +67,8 @@ response = requests.get(url)
 data = response.json()
 
 # Create a Folium map centered on Barcelona
-m = folium.Map(location=[41.3728027, 2.154058], zoom_start=13)
-
+m = folium.Map(location=[41.38879, 2.15899], zoom_start=13)
+ubis= []
 # Add markers to the map
 for ejemplo in data['locations']:
     empresa = ejemplo["network_name"]
@@ -62,22 +94,62 @@ for ejemplo in data['locations']:
         if estacion["port_status"][0]["status"] == "AVAILABLE":
             disponibles += 1
 
+    if disponibles >= n_charg:
+        ubis.append((lat, lon))
     # Aquí puedes realizar las operaciones que desees con los datos extraídos
-
     marker_color = get_marker_color(disponibles)
     marker_popup = get_marker_popup({
         'label': empresa,
-        'num': disponibles,
+        'num': f"{disponibles}/{len(ejemplo['stations'][0]['ports'])}",
         'dir': direccion,
         'dias': dias,
         'horario':horario,
         'acceso': acceso
     })
-    folium.Marker(
-        location=[lat, lon],
-        icon=folium.Icon(color=marker_color),
-        popup=marker_popup
-    ).add_to(m)
-# Display the map using Streamlit
-st.markdown("<h1 style='text-align: center;'>Electric Vehicle Chargers in Barcelona</h1>", unsafe_allow_html=True)
-folium_static(m)
+
+    folium.Marker(location=[lat, lon],icon=folium.Icon(color=marker_color,icon='plug', prefix='fa'), popup=marker_popup).add_to(m)
+
+
+if dire:
+    geocoding_url = f"https://nominatim.openstreetmap.org/search?q={dire}&format=json"
+    response = requests.get(geocoding_url)
+    results = response.json()
+
+    if len(results) > 0:
+        lat = float(results[0]['lat'])
+        lon = float(results[0]['lon'])
+
+        # Agregar un marcador en la ubicación ingresada
+
+        folium.Marker(
+            location=[lat, lon],
+            icon=folium.Icon(color='blue'),
+            popup=dire
+        ).add_to(m)
+
+        closest_point = min(ubis, key=lambda p: ((p[0] - lat)**2 + (p[1] - lon)**2)**0.5)
+
+        try:
+            routing_url = f"http://router.project-osrm.org/route/v1/driving/{lon},{lat};{closest_point[1]},{closest_point[0]}?overview=full&geometries=geojson"
+            response = requests.get(routing_url)
+            route_data = response.json()
+            route_geometry = route_data['routes'][0]['geometry']
+
+            route_style = {
+                'fillColor': 'blue',
+                'color': 'blue',
+                'weight': 5
+            }
+
+            # Agregar la ruta al mapa
+            folium.GeoJson(
+                route_geometry,
+                style_function=lambda x: route_style
+            ).add_to(m)
+            bounds = folium.GeoJson(route_geometry).get_bounds()
+            center = [(bounds[0][0] + bounds[1][0]) / 2, (bounds[0][1] + bounds[1][1]) / 2]
+            m.fit_bounds(route.get_bounds(), center=center)
+        except:
+            pass
+
+stf.folium_static(m, width=1300, height=600)
