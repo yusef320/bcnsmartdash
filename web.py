670e7880031e5845740b8b659ea5153dc05bc951
pablo-csv <83393209+pablo-csv@users.noreply.github.com>
2023-06-25 19:23:03 +0200

    Update web.py

--- web.py
+++ web.py
@@ -33,7 +33,7 @@ def get_marker_popup(station):
     return popup
 
 
-def grafico_circular(rojos, naranjas, verdes, graph_width):
+def grafico_circular(rojos, naranjas, verdes):
     #total = rojos + naranjas + verdes
     #rojos_porc = rojos*100/total
     #naranjas_porc = naranjas*100/total
@@ -42,14 +42,56 @@ def grafico_circular(rojos, naranjas, verdes, graph_width):
     valores = [rojos, naranjas, verdes]
     colores = ['#d33d2a', '#f59630', '#71ae26']  # Rojo, naranja, verde
 
-    # Crea la figura y el gráfico circular
+    # Crear la figura y el gráfico circular
     fig = go.Figure(data=[go.Pie(labels=labels, values=valores, marker=dict(colors=colores))])
 
-    # Configura el diseño del gráfico
-    fig.update_layout(title_text="% of chargers by number of available connectors",
-                      title_x=0.3, legend_x=0.7, width=graph_width*0.8)
+    # Configurar el diseño del gráfico
+    fig.update_layout(title_text="Proportion of chargers by number of available connectors",
+                      title_x=0.3, legend_x=0.7)
 
-    # Muestra el gráfico interactivo
+    # Mostrar el gráfico interactivo
+    st.plotly_chart(fig)
+    
+    
+def histogram_available(disponibles_list):
+    # Número de bins y figura
+    valor_max = max(disponibles_list)
+    print(valor_max)
+    
+    # Crear histograma y añadir etiquetas y título
+    fig = go.Figure(data=[go.Histogram(x=disponibles_list, nbinsx=valor_max*2, marker=dict(color='#183DF5'))])
+    fig.update_layout(
+        title_text="Available connectors distribution",
+        title_x=0.3,
+        xaxis_title="Number of available connectors",
+        yaxis_title="Number of chargers"
+    )
+    
+    # Deslizar interactivo
+    fig.update_traces(hovertemplate='there are %{y} chargers with %{x} available connectors')
+    
+    # Mostrar
+    st.plotly_chart(fig)
+
+
+def histogram_total(totales_list):
+    # Número de bins y figura
+    valor_max = max(totales_list)
+    print(valor_max)
+    
+    # Crear histograma y añadir etiquetas y título
+    fig = go.Figure(data=[go.Histogram(x=totales_list, nbinsx=valor_max, marker=dict(color='#183DF5'))])
+    fig.update_layout(
+        title_text="Total connectors distribution",
+        title_x=0.3,
+        xaxis_title="Number of total connectors",
+        yaxis_title="Number of chargers"
+    )
+    
+    # Deslizar interactivo
+    fig.update_traces(hovertemplate='there are %{y} chargers with %{x} total connectors')
+    
+    # Mostrar
     st.plotly_chart(fig)
 
 
@@ -61,6 +103,8 @@ st.set_page_config(
 
 st.markdown("# eBCN chargers", unsafe_allow_html=True)
 
+st.write("Gazing at Barcelona's charger network as never before")
+
 
 # Load the JSON data from the provided URL
 url = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8cdafa08-d378-4bf1-aad4-fafffe815940/resource/e3732605-4944-44da-b8a0-701df2ba73c3/download'
@@ -69,8 +113,8 @@ data = response.json()
 
 
 # option to expand and view statistics
-with st.expander("**Network real-time statistics**"):
-    graph_width = st.slider('Graph width', 300, 1500, value=1000)
+with st.expander("**Network real-time statistics***"):
+    #graph_width = st.slider('Graph width', 300, 1500, value=1000)
     disponibles_list, totales_list = [], []
     for ejemplo in data['locations']:
         disponibles = 0  # cargadores disponibles en esta estación
@@ -87,7 +131,23 @@ with st.expander("**Network real-time statistics**"):
             naranjas += 1
         else:
             verdes += 1
-    grafico_circular(rojos, naranjas, verdes, graph_width)
+    
+    st.write(f'Number of **chargers**: **{len(totales_list)}**')
+    st.write(f'**Available** connectors: **{sum(disponibles_list)}**')
+    st.write(f'**Total** connectors: **{sum(totales_list)}**')
+    st.write(f'Network **occupation** ratio: **{round(sum(disponibles_list)*100/sum(totales_list), 2)} %**')
+    
+    # Plot pie chart
+    grafico_circular(rojos, naranjas, verdes)
+    
+    # Plot available connectors histogram
+    histogram_available(disponibles_list)
+    
+    # Plot total connectors histogram
+    histogram_total(totales_list)
+    
+    # Note
+    st.write('*Note: This section performs better in computer devices')
 
 
 col1, col2, col3 = st.columns([3,1,1])
@@ -110,7 +170,6 @@ else:
 
 # Create a Folium map centered on Barcelona
 m = folium.Map(location=[41.38879, 2.15899], zoom_start=13)
-print('mapa creado')
 ubis= []
 # Add markers to the map
 for ejemplo in data['locations']:
